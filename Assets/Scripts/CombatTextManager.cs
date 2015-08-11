d61cde5bc91898acd163dab4f048c1f8ecfec1a3
nsmae0127 <nsmae0127@gmail.com>
2015-08-11 21:48:36 +0900

    Updated150811

---
+++ Assets/Scripts/CombatTextManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombatTextManager : MonoBehaviour
+{
+    public static float health;
+
+    private static CombatTextManager instance;
+
+    public GameObject textPrefab;
+
+    public RectTransform canvasTransform;
+
+    public static CombatTextManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<CombatTextManager>();
+            }
+
+            return instance;
+        }
+    }
+
+    public void CreateText(Vector3 position)
+    {
+        GameObject sct = (GameObject)Instantiate(textPrefab, position, Quaternion.identity);
+
+        sct.transform.SetParent(canvasTransform);
+        sct.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CombatTextManager.Instance.CreateText(transform.position);
+        }
+
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        transform.Translate(new Vector3(horizontal, vertical) * speed * Time.deltaTime);
+    }
+}
